49dcc338f265e029735048225630bd7e3259ecca
rasim hesenov <devbeyt989@gmail.com>
2025-11-20 08:59:06 +0400

    flutter basic tutorial finished

--- lib/main.dart
+++ lib/main.dart
@@ -20,31 +20,67 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(textTheme: GoogleFonts.acmeTextTheme()),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Test App"),
-          centerTitle: true,
-          backgroundColor: Colors.blueGrey,
-          leading: Text("Menu"),
+      home:Center(
+        child: Container(
+          color: Colors.indigo,
+          width: 100,
+          height: 100,
         ),
-         body: Container(
-          margin: EdgeInsets.all(20.0),
-          padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
-          color: Colors.cyan,
-          child: Text("TEST"),
-        ),
+      )
+
+
+      //  Scaffold(
+      //   appBar: AppBar(
+      //     title: Text("Test App"),
+      //     centerTitle: true,
+      //     backgroundColor: Colors.blueGrey,
+      //     leading: Text("Menu"),
+      //   ),
+      //    body: 
+        //  Column(
+        //   mainAxisAlignment: MainAxisAlignment.spaceAround,
+        //   crossAxisAlignment: CrossAxisAlignment.end,
+        //   children: [
+        //     Text("TEST"),
+        //     ElevatedButton(onPressed: (){}, child: Text("daxil ol")),
+        //     Container(
+        //       padding: EdgeInsets.all(20.0),
+        //       color: Colors.red,
+        //       child: Text("test"),
+        //     )
+        //   ],
+        //  ),
+        //  Row(
+        //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        //   crossAxisAlignment: CrossAxisAlignment.end,
+        //   children:[
+        //     Text("TEST"),
+        //     ElevatedButton(onPressed: (){}, child: Text("daxil ol")),
+        //     Container(
+        //     padding: EdgeInsets.all(30.0),
+        //     color: Colors.blue,
+        //     child: Text("Container"),
+        //   ),
+        //   ],
+        //  ),
+        //  Container(
+        //   margin: EdgeInsets.all(20.0),
+        //   padding: EdgeInsets.fromLTRB(40, 20, 40, 20),
+        //   color: Colors.cyan,
+        //   child: Text("TEST"),
+        // ),
         // Center(
         //   child: Image.asset("lib/assets/images/coder-in-dubai.png")
         // ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            setState(() {
-              count += 1;
-            });
-          },
-          child: Text("+"),
-        ),
-      ),
+      //   floatingActionButton: FloatingActionButton(
+      //     onPressed: () {
+      //       setState(() {
+      //         count += 1;
+      //       });
+      //     },
+      //     child: Text("+"),
+      //   ),
+      // ),
     );
   }
 }
